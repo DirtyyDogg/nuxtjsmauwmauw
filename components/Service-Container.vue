019e3aba8b09e0b2eae4a902aec87081fb7d6d10
DirtyyDogg <rbreddels@hotmail.com>
2020-05-28 18:16:17 +0000

    cardsss

--- components/Service-Container.vue
+++ components/Service-Container.vue
@@ -1,83 +1,19 @@
 <template>
-  <b-container class="containerService">
-    <b-row align-v="stretch" align-h="between" cols="1" cols-md="2" cols-lg="4">
-      <b-col>
-        <Slide1 />
-      </b-col>
-      <!--  -->
-      <b-col>
-        <Slide2 />
-      </b-col>
-      <!--  -->
-      <b-col>
-        <Slide3 />
-      </b-col>
-      <!--  -->
-      <b-col>
-        <Slide4 />
-      </b-col>
-    </b-row>
-  </b-container>
+  <section class="containerServicer">
+    <Slide />
+  </section>
 </template>
 
 <script>
-import Slide1 from '~/components/Slide-Cards/slide1'
-import Slide2 from '~/components/Slide-Cards/slide2'
-import Slide3 from '~/components/Slide-Cards/slide3'
-import Slide4 from '~/components/Slide-Cards/slide4'
+import Slide from '~/components/Slide-Cards/Slide'
+
 export default {
   components: {
-    Slide1, Slide2, Slide3, Slide4
+    Slide
   }
 }
 </script>
 
-<style lang="scss" scoped>
-@media screen and (min-width: 992px) {
-    div#bcolLast {
-    margin: 0 -83px 0 0;
-    }
-}
+<style lang="scss">
 
-// UP Styles
-.containerUP {
-  position: relative;
-  width: 100%;
-  min-height: 7cm;
-}
-
-.imageUP {
-  display: block;
-  width: 100%;
-  height: auto;
-}
-
-.overlayUP {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: #008CBA;
-  overflow: hidden;
-  width: 100%;
-  height: 0;
-//   height: 63px;
-  transition: .5s ease;
-}
-
-.containerUP:hover .overlayUP {
-  height: 100%;
-}
-
-.textUP {
-  color: white;
-  font-size: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
 </style>
---
+++ components/Slide-Cards/Slide.vue
@@ -0,0 +1,116 @@
+<template>
+  <!--  -->
+  <b-container class="containerService">
+    <b-row align-v="stretch" align-h="between" cols="1" cols-md="2" cols-lg="4">
+      <b-col>
+        <Slide1 />
+      </b-col>
+      <!--  -->
+      <b-col>
+        <Slide2 />
+      </b-col>
+      <!--  -->
+      <b-col>
+        <Slide3 />
+      </b-col>
+      <!--  -->
+      <b-col>
+        <Slide4 />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Slide1 from '~/components/Slide-Cards/slide1'
+import Slide2 from '~/components/Slide-Cards/slide2'
+import Slide3 from '~/components/Slide-Cards/slide3'
+import Slide4 from '~/components/Slide-Cards/slide4'
+export default {
+  components: {
+    Slide1, Slide2, Slide3, Slide4
+  }
+}
+</script>
+
+<style lang="scss">
+.containerUP {
+  position: relative;
+  width: 96%;
+  min-height: "20em";
+}
+
+.imageUP {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlayUP {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #008CBA;
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  transition: .5s ease;
+}
+
+.containerUP:hover .overlayUP {
+  height: 100%;
+}
+
+.textUP {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+// Mobile
+@media screen and (max-width: 500px) {
+   // UP Styles
+}
+
+// Large
+@media screen and (min-width: 992px) {
+
+}
+
+// Larger
+@media screen and (min-width: 1200px) {
+
+}
+
+// /
+//     .containerUP {
+//         position: relative;
+//         width: 100%;
+//         min-height: 7cm;
+//     }
+//     .overlayUP {
+//         position: absolute;
+//         bottom: 10px;
+//         left: 0;
+//         right: 0;
+//         background-color: #008CBA;
+//         overflow: hidden;
+//         width: 100%;
+//         height: 0;
+//         //   height: 63px;
+//         transition: .5s ease;
+//     }
+
+//     .containerUP:hover .overlayUP {
+//         height: 100%;
+//     }
+// }
+// /
+// // Mobile
+</style>
