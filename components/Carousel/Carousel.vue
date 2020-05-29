b2848109cfaa78a1b2e0d6d55f8bd4e3815c2b58
DirtyyDogg <rbreddels@hotmail.com>
2020-05-29 01:59:15 +0000

    ds

--- components/Carousel/Carousel.vue
+++ components/Carousel/Carousel.vue
@@ -34,7 +34,7 @@
         <b-carousel-slide>
           <template v-slot:img>
             <img
-              class="d-block img-fluid w-100"
+              class="d-block img-fluid w-100 h-100"
               width="1024"
               height="480"
               src="https://picsum.photos/1024/480/?image=55"
--- components/Slide-Cards/Slide.vue
+++ components/Slide-Cards/Slide.vue
@@ -41,7 +41,7 @@ export default {
     padding: 8vh;
 }
 .containerCards.container {
-    margin-top: 60px;
+    margin-top: 20px;
 }
 .containerUP {
   position: relative;
